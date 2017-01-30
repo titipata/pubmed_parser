67771d5bce6a8c8025a0381e274725a4120088e1
titipata <titipat.a@u.northwestern.edu>
2017-01-30 13:23:34 -0600

    release version 0.1, update description

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python
 from setuptools import setup
 
-descr = '''Parser for Pubmed Open-Access Subset XML Dataset'''
-
 if __name__ == "__main__":
     setup(
         name='pubmed_parser',
-        version='0.1.dev',
-        description='Python parser for PubMed open source data',
+        version='0.1',
+        description='Python parser for Pubmed Open-Access Subset and MEDLINE XML repository',
         long_description=open('README.md').read(),
         url='https://github.com/titipata/pubmed_parser',
         author='Titipat Achakulvisut',
