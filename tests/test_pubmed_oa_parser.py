d255a2f0dc5bb13524811cd01ca5082a57165580
Nils Herrmann <nils18@live.com.mx>
2024-06-02 13:46:03 +0200

    #110 Parse text inside journal-title nodes.

--- tests/test_pubmed_oa_parser.py
+++ tests/test_pubmed_oa_parser.py
@@ -39,6 +39,7 @@ def test_parse_pubmed_xml():
     assert parsed_xml.get("doi") == "10.1371/journal.pone.0046493"
     assert parsed_xml.get("subjects") == "Research Article; Biology; Biochemistry; Enzymes; Enzyme Metabolism; Lipids; Fatty Acids; Glycerides; Lipid Metabolism; Neutral Lipids; Metabolism; Lipid Metabolism; Proteins; Globular Proteins; Protein Classes; Recombinant Proteins; Biotechnology; Microbiology; Bacterial Pathogens; Bacteriology; Emerging Infectious Diseases; Host-Pathogen Interaction; Microbial Growth and Development; Microbial Metabolism; Microbial Pathogens; Microbial Physiology; Proteomics; Sequence Analysis; Spectrometric Identification of Proteins"  # noqa
     assert "Competing Interests: " in parsed_xml.get("coi_statement")
+    assert parsed_xml.get("journal") == "PLoS ONE"
 
 
 def test_parse_pubmed_paragraph():
