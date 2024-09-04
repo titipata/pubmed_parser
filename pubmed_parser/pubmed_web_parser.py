3ae0bb6c42eef8df7d2021d49e321100492b6796
Nils Herrmann <88451442+nils-herrmann@users.noreply.github.com>
2024-09-04 10:26:39 +0200

    Parse version_id and version_date in parse_xml_web() (close #134(

--- pubmed_parser/pubmed_web_parser.py
+++ pubmed_parser/pubmed_web_parser.py
@@ -60,7 +60,7 @@ def parse_pubmed_web_tree(tree):
         'title', 'abstract', 'journal', 'affliation' (string of affiliation with ';' separated),
         'authors' (string with ';' separated),
         'keywords' (keywords and MeSH terms from an XML -- if MeSH term it will be 'MeSH descriptor':'MeSH name')
-        'doi', 'year' 
+        'doi', 'pii', 'year', 'language', 'version_id', 'version_date'
     """
     if len(tree.xpath("//articletitle")) != 0:
         title = " ".join([title.text for title in tree.xpath("//articletitle")])
@@ -149,6 +149,13 @@ def parse_pubmed_web_tree(tree):
         language = language[0].text
     except IndexError:
         language = None
+    
+    medline_citation = tree.xpath('//medlinecitation')
+    try:
+        version_id = medline_citation[0].attrib.get('versionid')
+        version_date = medline_citation[0].attrib.get('versiondate')
+    except IndexError:
+        version_id, version_date = None, None
 
     dict_out = {
         "title": title,
@@ -160,7 +167,9 @@ def parse_pubmed_web_tree(tree):
         "doi": doi,
         "pii": pii,
         "year": year,
-        "language": language
+        "language": language,
+        "version_id": version_id,
+        "version_date": version_date,
     }
     return dict_out
 
@@ -199,6 +208,8 @@ def parse_xml_web(pmid, sleep=None, save_xml=False):
         'keywords': 'D000818:Animals;D005075:Biological Evolution;...',
         'doi': '10.1126/science.1060852',
         'year': '2001',
+        'version_id': None,
+        'version_date': None,
         'pmid': '11360989'
     }
     """
